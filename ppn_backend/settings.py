5dcc5fe2658918c6bfea2ce2da9c31eeb598dc60
T.D. Mees <t.d.mees@uu.nl>
2018-11-22 14:59:51 +0100

    Updated user setting in settings

--- ppn_backend/settings.py
+++ ppn_backend/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 from django.utils.translation import gettext_lazy as _
+from django.urls import reverse_lazy
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -85,6 +86,9 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'ppn_backend.wsgi.application'
 
+AUTH_USER_MODEL = 'main.User'
+
+LOGIN_URL = reverse_lazy('main:login')
 
 SESSION_COOKIE_NAME = "sessionid_admin"
 
