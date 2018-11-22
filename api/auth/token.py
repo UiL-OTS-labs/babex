dde999845a2cc4bb5a6dd77fa1d5116973d5a8a4
T.D. Mees <t.d.mees@uu.nl>
2018-11-22 13:50:51 +0100

    Cleaned up the JwtToken class

--- api/auth/token.py
+++ api/auth/token.py
@@ -1,22 +1,40 @@
+from django.conf import settings
+from django.core import exceptions
 import jwt
+import enum
+
+
+class Algorithms(enum.Enum):
+    SHA = 'HS512'
+    RSA = 'RSA512'
 
 
 class JwtToken:
 
     def __init__(self):
-        self._key = 'broodje_kaas'
-        #self._algorithm = "RS512"
-        self._algorithm = "HS512"
+        self._algorithm = None
+        self._encode_key = None
+        self._decode_key = None
 
-        # with open('jwtRS256.key', 'r') as keyfile:
-        #     self._private_key = keyfile.read()
-        #
-        # with open('jwtRS256.key.pub', 'r') as keyfile:
-        #     self._public_key = keyfile.read()
+        if hasattr(settings, 'JWT_ALGORITHM'):
+            self._algorithm = Algorithms(settings.JWT_ALGORITHM)
+        else:
+            raise exceptions.ImproperlyConfigured('JWT_ALGORITHM not set!')
+
+        if self._algorithm == Algorithms.RSA:
+            with open(settings.JWT_PRIVATE_KEY, 'r') as key_file:
+                self._encode_key = key_file.read()
+
+            with open(settings.JWT_PUBLIC_KEY, 'r') as key_file:
+                self._decode_key = key_file.read()
+
+        elif self._algorithm == Algorithms.SHA:
+            self._encode_key = settings.SECRET_KEY
+            self._decode_key = settings.SECRET_KEY
 
     def validate_token(self, token):
         try:
-            decoded = jwt.decode(token, self._key, algorithms=[self._algorithm])
+            decoded = jwt.decode(token, self._decode_key, algorithms=[self._algorithm.value])
         except jwt.DecodeError:
             return None
 
@@ -27,7 +45,7 @@ class JwtToken:
             "pk": user.pk
         }
 
-        return jwt.encode(payload, self._key, algorithm=self._algorithm)
+        return jwt.encode(payload, self._encode_key, algorithm=self._algorithm.value)
 
 
 JwtToken = JwtToken()
