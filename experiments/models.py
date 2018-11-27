54c37099c1bf101d81ee4d99f6bed51401324712
T.D. Mees <t.d.mees@uu.nl>
2018-11-27 10:58:37 +0100

    Added translations for all models fields and use those for the tables

--- experiments/models.py
+++ experiments/models.py
@@ -1,35 +1,60 @@
 from django.db import models
-from django.core.validators import  MinValueValidator
+from django.core.validators import MinValueValidator
+from django.utils.translation import ugettext_lazy as _
 
 
 class Experiment(models.Model):
 
-    name = models.TextField()
+    name = models.TextField(
+        _('experiment:attribute:name')
+    )
 
-    duration = models.TextField()
+    duration = models.TextField(
+        _('experiment:attribute:duration')
+    )
 
-    compensation = models.TextField()
+    compensation = models.TextField(
+        _('experiment:attribute:compensation')
+    )
 
-    additional_instructions = models.TextField()
+    additional_instructions = models.TextField(
+        _('experiment:attribute:additional_instructions')
+    )
 
-    location = models.TextField()
+    location = models.TextField(
+        _('experiment:attribute:location')
+    )
 
-    open = models.BooleanField()
+    open = models.BooleanField(
+        _('experiment:attribute:open')
+    )
 
-    # TODO: rename this to 'public'
-    visible = models.BooleanField()
+    public = models.BooleanField(
+        _('experiment:attribute:public')
+    )
 
-    participants_visible = models.BooleanField()
+    participants_visible = models.BooleanField(
+        _('experiment:attribute:participants_visible')
+    )
 
-    excluded_experiments = models.ManyToManyField("self")
+    excluded_experiments = models.ManyToManyField(
+        "self",
+        _('experiment:attribute:excluded_experiments')
+    )
 
 
 class TimeSlot(models.Model):
 
-    experiment = models.ForeignKey(Experiment, on_delete=models.CASCADE)
+    experiment = models.ForeignKey(
+        Experiment,
+        on_delete=models.CASCADE
+    )
 
-    datetime = models.DateTimeField()
+    datetime = models.DateTimeField(
+        _('time_slot:attribute:datetime')
+    )
 
     max_places = models.PositiveSmallIntegerField(
+        _('time_slot:attribute:max_places'),
         validators=[MinValueValidator(1)]
     )
