713c65151d98c1b8eb70e186edea6380176672fc
T.D. Mees <t.d.mees@uu.nl>
2018-11-27 15:21:54 +0100

    Added the defaultCriteria model

--- experiments/migrations/0005_auto_20181127_1308.py
+++ experiments/migrations/0005_auto_20181127_1308.py
@@ -14,12 +14,6 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
-            name='DefaultCriteria',
-            fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-            ],
-        ),
-        migrations.CreateModel(
             name='Location',
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
--- experiments/models.py
+++ experiments/models.py
@@ -93,7 +93,86 @@ class Experiment(models.Model):
 
 
 class DefaultCriteria(models.Model):
-    pass
+
+    MULTILINGUAL = (
+        ('Y', _('default_criteria:attribute:multilingual:yes')),
+        ('N', _('default_criteria:attribute:multilingual:no')),
+        ('I', _('experiments:globals:indifferent')),
+    )
+
+    SEX = (
+        ('M', _('default_criteria:attribute:sex:male')),
+        ('F', _('default_criteria:attribute:sex:female')),
+        ('I', _('experiments:globals:indifferent')),
+    )
+
+    HANDEDNESS = (
+        ('L', _('default_criteria:attribute:handedness:lefthanded')),
+        ('R', _('default_criteria:attribute:handedness:righthanded')),
+        ('I', _('experiments:globals:indifferent')),
+    )
+
+    DYSLEXIA = (
+        ('Y', _('default_criteria:attribute:dyslexia:yes')),
+        ('N', _('default_criteria:attribute:dyslexia:no')),
+        ('I', _('experiments:globals:indifferent')),
+    )
+
+    SOCIAL_STATUS = (
+        ('S', _('default_criteria:attribute:social_status:student')),
+        ('O', _('default_criteria:attribute:social_status:other')),
+        ('I', _('experiments:globals:indifferent')),
+    )
+
+    experiment = models.OneToOneField(
+        Experiment,
+        on_delete=models.CASCADE
+    )
+
+    language = models.TextField(
+        _('default_criteria:attribute:language'),
+        default='nl'
+    )
+
+    multilingual = models.CharField(
+        _('default_criteria:attribute:multilingual'),
+        choices=MULTILINGUAL,
+        max_length=1,
+    )
+
+    sex = models.CharField(
+        _('default_criteria:attribute:sex'),
+        choices=SEX,
+        max_length=1,
+    )
+
+    handedness = models.CharField(
+        _('default_criteria:attribute:handedness'),
+        choices=HANDEDNESS,
+        max_length=1,
+    )
+
+    dyslexia = models.CharField(
+        _('default_criteria:attribute:dyslexia'),
+        choices=DYSLEXIA,
+        max_length=1
+    )
+
+    social_status = models.CharField(
+        _('default_criteria:attribute:social_status'),
+        choices=SOCIAL_STATUS,
+        max_length=1
+    )
+
+    min_age = models.IntegerField(
+        _('default_criteria:attribute:min_age'),
+        validators=[MinValueValidator(-1)],
+    )
+
+    max_age = models.IntegerField(
+        _('default_criteria:attribute:max_age'),
+        validators=[MinValueValidator(-1)],
+    )
 
 
 class TimeSlot(models.Model):
