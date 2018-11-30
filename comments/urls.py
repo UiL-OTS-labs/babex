b5525f90ea379effc0cdb462d5039b7b1cb16af5
T.D. Mees <t.d.mees@uu.nl>
2018-11-30 16:13:56 +0100

    Added comment delete view

--- comments/urls.py
+++ comments/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
-from .views import CommentsHomeView
+from .views import CommentsHomeView, CommentsDeleteView
 
 
 app_name = 'comments'
 
 urlpatterns = [
     url(r'^$', CommentsHomeView.as_view(), name='home'),
+    url(r'(?P<pk>\d+)/delete/', CommentsDeleteView.as_view(), name='delete')
 ]
--- comments/views.py
+++ comments/views.py
@@ -1,4 +1,5 @@
 from django.views import generic
+from django.urls import reverse_lazy as reverse
 import braces.views as braces
 
 from .models import Comment
@@ -7,3 +8,9 @@ from .models import Comment
 class CommentsHomeView(braces.LoginRequiredMixin, generic.ListView):
     template_name = 'comments/index.html'
     model = Comment
+
+
+class CommentsDeleteView(braces.LoginRequiredMixin, generic.DeleteView):
+    model = Comment
+    success_url = reverse('comments:home')
+    template_name = 'comments/delete.html'
