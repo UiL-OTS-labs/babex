30ab3019442c952b00c194ec64c447bdaa6d5403
T.D. Mees <t.d.mees@uu.nl>
2018-11-30 12:17:05 +0100

    Added RedirectSuccessMessageMixin

--- main/views.py
+++ main/views.py
@@ -1,6 +1,21 @@
 from django.views import generic
+from django.contrib import messages
 import braces.views as braces
 
 
 class HomeView(braces.LoginRequiredMixin, generic.TemplateView):
     template_name = 'main/index.html'
+
+
+class RedirectSuccessMessageMixin:
+    success_message = ''
+
+    def get(self, *args, **kwargs):
+        response = super(RedirectSuccessMessageMixin, self).get(*args, **kwargs)
+
+        messages.success(self.request, self.success_message)
+
+        return response
+
+    def get_success_message(self):
+        return self.success_message
