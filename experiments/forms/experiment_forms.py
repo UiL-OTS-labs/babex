f1ace12899827305bf1799a4ea4002c34345490e
T.D. Mees <t.d.mees@uu.nl>
2018-11-30 12:53:22 +0100

    Experiments cannot exclude themself anymore

--- experiments/forms/experiment_forms.py
+++ experiments/forms/experiment_forms.py
@@ -8,8 +8,18 @@ class ExperimentForm(forms.ModelForm):
         model = Experiment
         fields = '__all__'
         widgets = {
-            'name': forms.TextInput,
-            'duration': forms.TextInput,
+            'name':         forms.TextInput,
+            'duration':     forms.TextInput,
             'compensation': forms.TextInput,
         }
 
+    def __init__(self, *args, **kwargs):
+        super(ExperimentForm, self).__init__(*args, **kwargs)
+
+        # If we are updating an experiment, make sure you cannot exclude the
+        # experiment you are updating!
+        if self.instance:
+            other_experiments = Experiment.objects.exclude(pk=self.instance.pk)
+            self.fields['excluded_experiments'].choices = [
+                (x.pk, x.name) for x in other_experiments
+            ]
