ac40e376ea486e651db2681e24de5654bdb01a32
T.D. Mees <t.d.mees@uu.nl>
2018-12-11 13:20:53 +0100

    Added a Participant detail view

--- participants/urls.py
+++ participants/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
-from .views import ParticipantsHomeView
+from .views import ParticipantsHomeView, ParticipantDetailView
 
 
 app_name = 'participants'
 
 urlpatterns = [
     url(r'^$', ParticipantsHomeView.as_view(), name='home'),
+    url(r'^(?P<pk>\d+)/$', ParticipantDetailView.as_view(), name='detail'),
 ]
--- participants/views.py
+++ participants/views.py
@@ -7,3 +7,8 @@ from .models import Participant
 class ParticipantsHomeView(braces.LoginRequiredMixin, generic.ListView):
     template_name = 'participants/index.html'
     model = Participant
+
+
+class ParticipantDetailView(braces.LoginRequiredMixin, generic.DetailView):
+    model = Participant
+    template_name = 'participants/detail.html'
