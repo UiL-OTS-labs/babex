0ff538a5adf04e6c3736813d80126b81480802ce
T.D. Mees <t.d.mees@uu.nl>
2018-12-11 12:23:31 +0100

    Updated the participant model with name's and proper blank usage

--- participants/models.py
+++ participants/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
 from api.auth.models import ApiUser
+from experiments.models.criteria_models import Criterium
 
 
 # TODO: encrypted fields!
@@ -25,36 +26,95 @@ class Participant(models.Model):
         ('other', _('participant:attribute:social_role:other')),
     )
 
-    email = models.EmailField()
+    email = models.EmailField(
+        _('participant:attribute:email'),
+    )
 
-    name = models.TextField()
+    name = models.TextField(
+        _('participant:attribute:name'),
+        blank=True,
+        null=True,
+    )
 
-    language = models.TextField()
+    language = models.TextField(
+        _('participant:attribute:language'),
+    )
 
-    dyslexic = models.BooleanField()
+    dyslexic = models.BooleanField(
+        _('participant:attribute:dyslexic'),
+    )
 
-    birth_date = models.DateField()
+    birth_date = models.DateField(
+        _('participant:attribute:birth_date'),
+        blank=True,
+        null=True,
+    )
 
-    multilingual = models.BooleanField()
+    # NOTE: When updating to Django 2.1 you should change this to a regular
+    # BooleanField
+    multilingual = models.NullBooleanField(
+        _('participant:attribute:multilingual'),
+        blank=True,
+        null=True,
+    )
 
-    phonenumber = models.TextField()
+    phonenumber = models.TextField(
+        _('participant:attribute:phonenumber'),
+        blank=True,
+        null=True,
+    )
 
-    handedness = models.TextField(choices=HANDEDNESS)
+    handedness = models.TextField(
+        _('participant:attribute:handedness'),
+        choices=HANDEDNESS,
+        blank=True,
+        null=True,
+    )
 
-    sex = models.TextField(choices=SEX)
+    sex = models.TextField(
+        _('participant:attribute:sex'),
+        choices=SEX,
+        blank=True,
+        null=True,
+    )
 
-    social_status = models.TextField(choices=SOCIAL_STATUS)
+    social_status = models.TextField(
+        _('participant:attribute:social_status'),
+        choices=SOCIAL_STATUS,
+        blank=True,
+        null=True,
+    )
 
-    email_subscription = models.BooleanField(default=False)
+    email_subscription = models.BooleanField(
+        _('participant:attribute:email_subscription'),
+        default=False,
+    )
 
-    capable = models.BooleanField(default=True)
+    capable = models.BooleanField(
+        _('participant:attribute:capable'),
+        default=True,
+    )
 
-    api_user = models.OneToOneField(ApiUser, null=True, blank=True, on_delete=models.SET_NULL)
+    api_user = models.OneToOneField(
+        ApiUser,
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+    )
 
 
 class SecondaryEmail(models.Model):
-    email = models.EmailField()
+    email = models.EmailField(
+        _('secondary_email:attribute:email'),
+    )
 
     participant = models.ForeignKey(Participant, on_delete=models.CASCADE)
 
 
+# class CriteriaAnswer(models.Model):
+#
+#     participant = models.ForeignKey(Participant, on_delete=models.CASCADE)
+#
+#     criteria = models.ForeignKey(Criterium, on_delete=models.CASCADE)
+#
+#     answer = models.TextField()
