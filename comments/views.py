418810a49ff998502e6cf94077461676c607b4c7
T.D. Mees <t.d.mees@uu.nl>
2018-12-13 12:42:31 +0100

    Optimized the comment homepage for large amounts of data

--- comments/views.py
+++ comments/views.py
@@ -9,6 +9,12 @@ class CommentsHomeView(braces.LoginRequiredMixin, generic.ListView):
     template_name = 'comments/index.html'
     model = Comment
 
+    def get_queryset(self):
+        """Return a queryset that's been instructed to also pull the related
+        objects. This makes this page around 10 times faster"""
+        return self.model.objects.select_related('participant', 'leader',
+                                                 'experiment')
+
 
 class CommentsDeleteView(braces.LoginRequiredMixin, generic.DeleteView):
     model = Comment
