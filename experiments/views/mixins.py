feb600022c90a8d57b31ae202a84ad1db66f6712
T.D. Mees <t.d.mees@uu.nl>
2018-12-14 09:02:04 +0100

    The ExperimentObjectMixin can now be configured to use prefetching

--- experiments/views/mixins.py
+++ experiments/views/mixins.py
@@ -18,8 +18,19 @@ class ExperimentObjectMixin:
     """
     experiment_kwargs_name = 'experiment'
 
+    experiment_select_related = []
+    experiment_prefetch_related = []
+
     @cached_property
     def experiment(self):
         pk = self.kwargs.get(self.experiment_kwargs_name)
 
-        return Experiment.objects.get(pk=pk)
+        qs = Experiment.objects.get_queryset()
+
+        if self.experiment_select_related:
+            qs = qs.select_related(*self.experiment_select_related)
+
+        if self.experiment_prefetch_related:
+            qs = qs.prefetch_related(*self.experiment_prefetch_related)
+
+        return qs.get(pk=pk)
