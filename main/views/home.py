c5c4a0f8eba575e90e0d094491181894a29d3dbe
T.D. Mees <t.d.mees@uu.nl>
2019-03-29 12:38:23 +0100

    Added more info to the homepage

--- main/views/home.py
+++ main/views/home.py
@@ -1,6 +1,36 @@
 import braces.views as braces
 from django.views import generic
 
+from experiments.models import Experiment
+
+from django.db.models import Count, Sum
+
 
 class HomeView(braces.LoginRequiredMixin, generic.TemplateView):
     template_name = 'main/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(HomeView, self).get_context_data(**kwargs)
+
+        context['num_proposals'] = Experiment.objects.filter(open=True).count()
+        context['num_free_places'] = self._get_free_slots()
+
+        return context
+
+    def _get_free_slots(self):
+        qs = Experiment.objects.filter(open=True).select_related('location')
+
+        sum_places = Sum('timeslot__max_places')
+        count_participants = Count('timeslot__appointments')
+
+        experiments = qs.annotate(
+            n_places=sum_places,
+            n_participants=count_participants,
+        )
+
+        free_slots = 0
+
+        for experiment in experiments:
+            free_slots += experiment.n_places - experiment.n_participants
+
+        return free_slots
