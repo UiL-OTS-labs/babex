6cdd8d0ce207a91a6357ac91d5be6612519c11a4
T.D. Mees <t.d.mees@uu.nl>
2019-04-15 13:44:13 +0200

    Added a util method for adding system comments

---
+++ comments/utils.py
@@ -0,0 +1,29 @@
+from typing import Union, List
+
+from comments.models import Comment
+from participants.models import Participant
+
+
+def add_system_comment(
+        participants: Union[List[Participant], Participant],
+        comment: str
+) -> None:
+    """
+    Adds a system comment for a given participant or a list of participant.
+
+    If multiple participants are given, the comment will be the same for all
+    of them.
+
+    :param participants: Either a single Participant object or a list of them
+    :param comment: the comment to add.
+    """
+
+    if not isinstance(participants, list):
+        participants = [participants]
+
+    for participant in participants:
+        c = Comment()
+        c.participant = participant
+        c.comment = comment
+        c.system_comment = True
+        c.save()
