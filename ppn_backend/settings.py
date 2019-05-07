35ba4fe6be07d3d72ff2fdfe3b42a89434ea3672
T.D. Mees <t.d.mees@uu.nl>
2019-05-07 14:48:58 +0200

    Added django_extensions

--- ppn_backend/settings.py
+++ ppn_backend/settings.py
@@ -42,6 +42,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # Django extensions
+    'django_extensions',
+
     # django-simple-menu
     'menu',
 
