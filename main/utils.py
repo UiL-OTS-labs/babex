a6f6ff7abd4748648d3239359a4efafd9c42c939
T.D. Mees <t.d.mees@uu.nl>
2019-05-15 10:56:10 +0200

    Added the ability to add custom context for either the plain or html versions of an email

--- main/utils.py
+++ main/utils.py
@@ -9,7 +9,9 @@ from .models import User
 
 
 def send_template_email(recipient_list: list, subject: str, template: str,
-                        template_context: dict, from_email: str = None) -> None:
+                        template_context: dict, from_email: str = None,
+                        plain_context: dict = None, html_context: dict = None
+                        ) -> None:
     """
     Light wrapper for Django's send_mail function. The main addition: this
     function handles the template rendering for you. Just specify the template
@@ -23,18 +25,44 @@ def send_template_email(recipient_list: list, subject: str, template: str,
 
     Function call: send_template_email(template='app/test', *args, **kwargs)
 
+    html_context and plain_context can be used to override something in the
+    regular template_context for either the html version of the plain text
+    version. (Note: you can also add a key that does not exist in
+    template_context)
+
     :param recipient_list: A list of recipients
     :param subject: Email subject
     :param template: Template name, without extension
     :param template_context: Any context variables for the templates
     :param from_email: FROM header. If absent, settings.FROM_EMAIL will be used
+    :param html_context: Optional dict with context specifically for HTML
+    :param plain_context: Optional dict with context specifically for plaintext
     """
+    if plain_context is None:
+        plain_context = {}
+    if html_context is None:
+        html_context = {}
+
     # Override so that all emails will be parsed with dutch set
     old_lang = translation.get_language()
     translation.activate('nl')
 
-    plain_body = render_to_string('{}.txt'.format(template), template_context)
-    html_body = render_to_string('{}.html'.format(template), template_context)
+    # Create the context for both the plain text email
+    plain_text_context = template_context.copy()
+    plain_text_context.update(plain_context)
+
+    # And now the same for the HTML version
+    html_text_context = template_context.copy()
+    html_text_context.update(html_context)
+
+    plain_body = render_to_string(
+        '{}.txt'.format(template),
+        plain_text_context
+    )
+    html_body = render_to_string(
+        '{}.html'.format(template),
+        html_text_context
+    )
 
     # revert to the original language
     translation.activate(old_lang)
@@ -53,7 +81,10 @@ def send_template_email(recipient_list: list, subject: str, template: str,
 def send_personalised_mass_mail(datatuple: List[Tuple[str, dict, List[str]]],
                                 template: str,
                                 template_context: dict,
-                                from_email: str = None) -> None:
+                                from_email: str = None,
+                                plain_context: dict = None,
+                                html_context: dict = None
+                                ) -> None:
     """
     Given a datatuple of (subject, personal_context, recipient_list),
     send each message to each recipient list.
@@ -61,11 +92,23 @@ def send_personalised_mass_mail(datatuple: List[Tuple[str, dict, List[str]]],
     personal_context and template_context will be merged together to create
     a personalised email.
 
+    html_context and plain_context can be used to override something in the
+    regular template_context for either the html version of the plain text
+    version. (Note: you can also add a key that does not exist in
+    template_context)
+
     :param datatuple: A tuple of tuples: (subject, personal_context, recipient_list)
     :param template: Template name, without extension
     :param template_context: Any context variables for the templates
     :param from_email: FROM header. If absent, settings.FROM_EMAIL will be used
+    :param html_context: Optional dict with context specifically for HTML
+    :param plain_context: Optional dict with context specifically for plaintext
     """
+    if plain_context is None:
+        plain_context = {}
+    if html_context is None:
+        html_context = {}
+
     messages = []
     from_email = from_email or settings.FROM_EMAIL
     connection = get_connection()
@@ -75,13 +118,26 @@ def send_personalised_mass_mail(datatuple: List[Tuple[str, dict, List[str]]],
     translation.activate('nl')
 
     for subject, personal_context, recipient_list in datatuple:
+        # Copy the personal context and extend it with the generic context
         context = personal_context.copy()
         context.update(template_context)
 
-        plain_body = render_to_string('{}.txt'.format(template),
-                                      context)
-        html_body = render_to_string('{}.html'.format(template),
-                                     context)
+        # Copy our newly made context, and extend it with the plain_context
+        plain_text_context = context.copy()
+        plain_text_context.update(plain_context)
+
+        # And now the same for the HTML version
+        html_text_context = context.copy()
+        html_text_context.update(html_context)
+
+        plain_body = render_to_string(
+            '{}.txt'.format(template),
+            plain_text_context
+        )
+        html_body = render_to_string(
+            '{}.html'.format(template),
+            html_text_context
+        )
 
         message = EmailMultiAlternatives(subject, plain_body, from_email,
                                          recipient_list, connection=connection)
