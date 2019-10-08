d0d685c90620e1c60f86a96f2502860ce8295c31
Ty Mees <t.d.mees@uu.nl>
2019-10-08 14:24:13 +0200

    Clarified a bit

--- api/utils/common.py
+++ api/utils/common.py
@@ -1,6 +1,9 @@
 
 def x_or_else(x, y):
-    """If x is not None/empty string, return x. Else, return y"""
+    """If x is not None/empty string, return x. Else, return y.
+    (This is not the same as 'x = x or y', as this will also return x when
+    'x == False')
+    """
     if x is None or x == '':
         return y
 
