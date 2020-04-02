056ca81d9f8403f8716725ddf6dd867e693f9c2b
Mees, T.D. (Ty) <t.d.mees@uu.nl>
2020-04-02 12:03:54 +0200

    Api Auth now has complete support for Django Admin

--- api/auth/models.py
+++ api/auth/models.py
@@ -53,6 +53,14 @@ class ApiUser(models.Model):
 
     is_ldap_account = models.BooleanField(default=False)
 
+    date_joined = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    last_login = models.DateTimeField(
+        null=True
+    )
+
     @property
     def is_leader(self):
         return hasattr(self, 'leader')
