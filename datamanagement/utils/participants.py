39e4bbd82bc0d487022a938aad52a3f8c1b3b37c
Mees, T.D. (Ty) <t.d.mees@uu.nl>
2020-05-26 14:22:03 +0200

    Moved audit logging & participant deletion to the utils package

--- datamanagement/utils/participants.py
+++ datamanagement/utils/participants.py
@@ -4,6 +4,8 @@ from typing import List, Tuple
 
 from datamanagement.utils.common import get_threshold_years_ago
 from participants.models import Participant
+from auditlog.utils.log import log as log_to_auditlog
+from auditlog.enums import Event, UserType
 
 
 def get_participants_with_appointments() -> List[Tuple[Participant, datetime, int]]:
@@ -16,8 +18,6 @@ def get_participants_with_appointments() -> List[Tuple[Participant, datetime, in
         newest_appointment = participant.appointments.order_by(
             '-timeslot__datetime'
         ).first()
-
-        print(newest_appointment)
 
         if newest_appointment.timeslot.datetime < threshold:
             out.append(
@@ -36,3 +36,23 @@ def get_participants_without_appointments() -> List[Participant]:
         appointments=None,
         created__lte=get_threshold_years_ago('participants_without_appointment')
     ))
+
+
+def delete_participant(participant: Participant, user) -> bool:
+    if participant not in get_participants_without_appointments():
+        return False
+
+    log_to_auditlog(
+        Event.DELETE_DATA,
+        "Deleted participant '{}'".format(participant),
+        user,
+        UserType.ADMIN,
+    )
+
+    # Delete the account as well, unless the account is also a leader
+    if participant.api_user and not participant.api_user.leader:
+        participant.api_user.delete()
+
+    participant.delete()
+
+    return True
