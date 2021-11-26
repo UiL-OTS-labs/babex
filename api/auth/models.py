38aefcfc54cb23a813324c9bc7babad09755522f
Mees, T.D. (Ty) <t.d.mees@uu.nl>
2021-11-26 15:55:36 +0100

    Update check_password with latest django version of that function

--- api/auth/models.py
+++ api/auth/models.py
@@ -83,9 +83,10 @@ class ApiUser(models.Model):
         Return a boolean of whether the raw_password was correct. Handles
         hashing formats behind the scenes.
         """
-
         def setter(raw_password):
             self.set_password(raw_password)
+            # Password hash upgrades shouldn't be considered password changes.
+            self._password = None
             self.save(update_fields=["password"])
 
         return check_password(raw_password, self.password, setter)
