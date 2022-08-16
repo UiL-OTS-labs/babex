564f350d724b64ddc874d3b34021d276460540fa
ben <b.bonfil@uu.nl>
2022-08-16 17:24:16 +0200

    support for vue hot reload

--- babex-vue/vue.config.js
+++ babex-vue/vue.config.js
@@ -1,5 +1,12 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
     transpileDependencies: true,
-    outputDir: '../main/static/vue'
+    outputDir: '../main/static/vue',
+    publicPath: 'http://localhost:8081/',
+
+    devServer: {
+        headers: {
+            "Access-Control-Allow-Origin": "\*"
+        },
+    },
 })
