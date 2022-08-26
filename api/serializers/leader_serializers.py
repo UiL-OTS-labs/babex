623a286ef7d8b1ffdda68b79e08332d365c4d234
ben <b.bonfil@uu.nl>
2022-08-26 09:57:17 +0200

    repaired leader serializer

--- api/serializers/leader_serializers.py
+++ api/serializers/leader_serializers.py
@@ -1,6 +1,5 @@
 from rest_framework import serializers
 
-from api.auth.serializers import ApiUserSerializer
 from leaders.models import Leader
 
 
@@ -8,16 +7,11 @@ class LeaderSerializer(serializers.ModelSerializer):
     class Meta:
         model = Leader
         fields = [
-            'email', 'name', 'phonenumber', 'api_user', 'id'
+            'email', 'name', 'phonenumber', 'user', 'id'
         ]
         depth = 1
 
     email = serializers.SerializerMethodField()
 
-    api_user = serializers.SerializerMethodField()
-
     def get_email(self, object):
-        return object.api_user.email
-
-    def get_api_user(self, object):
-        return ApiUserSerializer(object.api_user).data
+        return object.user.email
