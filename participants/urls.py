11684f167fe3b17d0f65c939acb7a54bfe88c61f
Maarten Duijndam <m.j.a.duijndam@uu.nl>
2022-11-14 14:42:54 +0100

    Make participants menu a bit more fine grained

--- participants/urls.py
+++ participants/urls.py
@@ -8,6 +8,7 @@ app_name = 'participants'
 
 urlpatterns = [
     path('', ParticipantsHomeView.as_view(), name='home'),
+    path('demographics/', ParticipantsHomeView.as_view(), name='demographics'), #temporary stub in order to render the menu. Needs new view!
     path('<int:pk>/', ParticipantDetailView.as_view(), name='detail'),
     path('<int:pk>/edit/', ParticipantUpdateView.as_view(), name='edit'),
     path('<int:pk>/del/', ParticipantDeleteView.as_view(), name='delete'),
