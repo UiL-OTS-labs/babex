ba476f9f3a7b3633d75b3f28ef8a3bb6ea832ef6
ben <b.bonfil@uu.nl>
2022-11-28 10:53:45 +0100

    renamed frontdesk->parent

--- parent/parent/wsgi.py
+++ parent/parent/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for frontdesk project.
+WSGI config for parent project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'frontdesk.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'parent.settings')
 
 application = get_wsgi_application()
