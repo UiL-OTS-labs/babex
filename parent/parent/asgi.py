eb4d1c674346679f3f277224b652d1d2c4ed418b
ben <b.bonfil@uu.nl>
2022-11-28 10:53:45 +0100

    renamed frontdesk->parent

--- parent/parent/asgi.py
+++ parent/parent/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for frontdesk project.
+ASGI config for parent project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'frontdesk.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'parent.settings')
 
 application = get_asgi_application()
