0efcf143ae00357f77e2f7cad7736a50cdcbb9a5
ben <b.bonfil@uu.nl>
2022-12-12 15:21:36 +0100

    added an integration test for custom confirmation email

--- lab/integration_tests/test_invite.py
+++ lab/integration_tests/test_invite.py
@@ -1,8 +1,10 @@
-from datetime import date
+from datetime import date, datetime
+import time
 import pytest
+from django.core import mail
 
 from main.models import User
-from experiments.models import Experiment, DefaultCriteria
+from experiments.models import Appointment, DefaultCriteria, TimeSlot
 from leaders.models import Leader
 from participants.models import Participant
 
@@ -35,6 +37,21 @@ def sample_leader(db):
     yield Leader.objects.create(name='Leader McLeader',
                                 phonenumber='23456789',
                                 user=user)
+
+
+@pytest.fixture
+def sample_appointment(sample_experiment, sample_participant, sample_leader, db):
+    timeslot = TimeSlot.objects.create(
+        start=datetime(2023, 1, 1, 9, 0),
+        end=datetime(2023, 1, 1, 10, 0),
+        experiment=sample_experiment,
+        max_places=1
+    )
+    yield Appointment.objects.create(
+        participant=sample_participant,
+        experiment=sample_experiment,
+        timeslot=timeslot,
+        leader=sample_leader)
 
 
 def test_experiment_list(sb, sample_experiment, sample_participant, as_admin):
@@ -77,3 +94,45 @@ def test_schedule_appointment(sb, sample_experiment, sample_participant, sample_
     # appointment should contain both participant and leader names
     sb.assert_text('Baby McBaby', '.fc-event')
     sb.assert_text('Leader McLeader', '.fc-event')
+
+    # check that a confirmation email was sent
+    sb.assertEqual(len(mail.outbox), 1)
+    sb.assertEqual(mail.outbox[0].to[0], sample_participant.email)
+
+
+def test_schedule_appointment_edit_email(sb, sample_experiment, sample_participant, sample_leader, as_admin):
+    sample_experiment.leaders.add(sample_leader)
+
+    sb.click('a:contains(Experiments)')
+    sb.click('a:contains(Overview)')
+    sb.click('a:contains(Invite)')
+    sb.click('a.icon-phone')
+    sb.click('button:contains(Schedule)')
+
+    # pick time
+    sb.click(f'td[data-date="{date.today()}"]')
+    sb.click('td.fc-timegrid-slot-lane[data-time="10:00:00"]')
+    sb.click('button:contains(Next)')
+
+    # pick leader
+    sb.select_option_by_text('.modal-content select', 'Leader McLeader')
+    # choose to edit mail
+    sb.click('label:contains("Edit mail")')
+    sb.click('button:contains(Confirm)')
+
+    sb.wait_for_element('.tox-tinymce')
+
+    # set tinymce editor with a custon email string
+    test_email = '<em>this is a test email</em>'
+    test_email_plain = 'this is a test email'
+    sb.execute_script('tinymce.activeEditor.setContent("{}")'.format(test_email))
+
+    sb.assertEqual(len(mail.outbox), 0)
+    sb.click('button:contains(Confirm)')
+    sb.wait_for_element_not_visible('button:contains("Confirm")')
+
+    # check the email was sent
+    sb.assertEqual(len(mail.outbox), 1)
+    sb.assertEqual(mail.outbox[0].to[0], sample_participant.email)
+    sb.assertIn(test_email_plain, mail.outbox[0].body)
+    sb.assertIn(test_email, mail.outbox[0].alternatives[0][0])
