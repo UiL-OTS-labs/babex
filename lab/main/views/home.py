e49f37b279f01abdc5ba04ba458fa96e0fd20787
ben <b.bonfil@uu.nl>
2022-12-20 13:28:21 +0100

    added some content to home view

--- lab/main/views/home.py
+++ lab/main/views/home.py
@@ -1,40 +1,18 @@
 import braces.views as braces
 from django.views import generic
 
-from experiments.models import Experiment
-
-from django.db.models import Count, Sum
-from django.db.models.functions import Now
+from experiments.models.invite_models import Call
 
 
 class HomeView(braces.LoginRequiredMixin, generic.TemplateView):
     template_name = 'main/index.html'
 
-    def get_context_data(self, **kwargs):
-        context = super(HomeView, self).get_context_data(**kwargs)
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(**kwargs)
 
-        context['experiments'] = self._get_experiments()
+        user = self.request.user
+        context['experiments'] = user.leader.experiments.all()
+        context['call_back'] = set(call.participant for call in
+                                   user.leader.call_set.filter(status=Call.CallStatus.CALLBACK))
 
         return context
-
-    def _get_free_slots(self):
-        qs = Experiment.objects.filter(open=True).select_related(
-            'location').filter(timeslot__datetime__gte=Now())
-
-        sum_places = Sum('timeslot__max_places')
-        count_participants = Count('timeslot__appointments')
-
-        experiments = qs.annotate(
-            n_places=sum_places,
-            n_participants=count_participants,
-        )
-
-        free_slots = 0
-
-        for experiment in experiments:
-            free_slots += experiment.n_places - experiment.n_participants
-
-        return free_slots
-
-    def _get_experiments(self):
-        return Experiment.objects.filter(open=True)
