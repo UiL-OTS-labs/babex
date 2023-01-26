680e35b2b2d3051b70ec871def8ef10971b156cd
ben <b.bonfil@uu.nl>
2023-01-26 13:47:49 +0100

    black

--- lab/experiments/utils/exclusion.py
+++ lab/experiments/utils/exclusion.py
@@ -10,35 +10,36 @@ from typing import List, Optional
 
 from ageutil import age
 
-from experiments.models import Experiment, ExperimentCriterion, DefaultCriteria
+from experiments.models import (
+    Appointment,
+    DefaultCriteria,
+    Experiment,
+    ExperimentCriterion,
+)
 from experiments.models.invite_models import Call
 from participants.models import Participant
 
 # List of vars that can have the same values as the participant model
 # variables, with an indifferent option
 indifferentable_vars = [
-    'language',
-    'sex',
+    "language",
+    "sex",
 ]
 
 
-def get_eligible_participants_for_experiment(
-        experiment: Experiment, on_mailinglist: bool = True) -> List[Participant]:
+def get_eligible_participants_for_experiment(experiment: Experiment, on_mailinglist: bool = True) -> List[Participant]:
     """
     This function produces a list of participants that can take part in
     the provided experiment.
     """
     default_criteria = experiment.defaultcriteria
-    specific_experiment_criteria = \
-        experiment.experimentcriterion_set.select_related(
-            'criterion'
-        )
+    specific_experiment_criteria = experiment.experimentcriterion_set.select_related("criterion")
 
     # Base filters: a participant should be capable, and by default be on the
     # mailing list
     filters = {
-        'email_subscription': on_mailinglist,
-        'capable':            True,
+        "email_subscription": on_mailinglist,
+        "capable": True,
     }
 
     # Build the rest of the filters
@@ -48,13 +49,9 @@ def get_eligible_participants_for_experiment(
     # marked as an exclusion criteria
     participants = Participant.objects.exclude(
         appointments__experiment__in=experiment.excluded_experiments.all()
-    ).exclude(
-        # Exclude all participants who have already signed up
-        appointments__experiment=experiment
     ).prefetch_related(
-        'secondaryemail_set',  # Used in the invite page template!
-        'criterionanswer_set'
-    )
+        "secondaryemail_set", "criterionanswer_set"
+    )  # Used in the invite page template!
 
     # List of all allowed participants
     filtered = []
@@ -85,18 +82,14 @@ def get_eligible_participants_for_experiment(
     return filtered
 
 
-def check_participant_eligible(experiment: Experiment, participant:
-Participant) -> bool:
+def check_participant_eligible(experiment: Experiment, participant: Participant) -> bool:
     """
     This function checks if a given participant can participate in a given
     experiment
     """
 
     default_criteria = experiment.defaultcriteria
-    specific_experiment_criteria = \
-        experiment.experimentcriterion_set.select_related(
-            'criterion'
-        )
+    specific_experiment_criteria = experiment.experimentcriterion_set.select_related("criterion")
 
     filters = build_exclusion_filters(default_criteria)
 
@@ -106,8 +99,7 @@ Participant) -> bool:
     if should_exclude_by_age(participant, default_criteria):
         return False
 
-    if _should_exclude_by_specific_criteria(participant,
-                                            specific_experiment_criteria):
+    if _should_exclude_by_specific_criteria(participant, specific_experiment_criteria):
         return False
 
     return True
@@ -125,17 +117,17 @@ def build_exclusion_filters(default_criteria, filters=None) -> dict:
         filters = {}
 
     for var in indifferentable_vars:
-        if getattr(default_criteria, var) != 'I':
+        if getattr(default_criteria, var) != "I":
             filters[var] = getattr(default_criteria, var)
 
     # Dyslexia is always a filter
-    expected_value = default_criteria.dyslexia == 'Y'
-    filters['dyslexic_parent'] = expected_value
+    expected_value = default_criteria.dyslexia == "Y"
+    filters["dyslexic_parent"] = expected_value
 
     # Rewrite this expected to a boolean value, as it's stored as a boolean
-    if default_criteria.multilingual != 'I':
-        expected_value = default_criteria.multilingual == 'Y'
-        filters['multilingual'] = expected_value
+    if default_criteria.multilingual != "I":
+        expected_value = default_criteria.multilingual == "Y"
+        filters["multilingual"] = expected_value
 
     return filters
 
@@ -162,8 +154,7 @@ def check_default_criteria(participant: Participant, filters: dict) -> list:
     return failed_criteria
 
 
-def _should_exclude_by_specific_criteria(participant: Participant,
-                                         specific_experiment_criteria) -> bool:
+def _should_exclude_by_specific_criteria(participant: Participant, specific_experiment_criteria) -> bool:
     """
     Determines if a participant should be excluded based upon their
     :param participant:
@@ -181,20 +172,15 @@ def _should_exclude_by_specific_criteria(participant: Participant,
             continue
 
         # Get the experiment criterion
-        specific_criterion = _get_specific_criterion(
-            specific_experiment_criteria,
-            specific_criterion_answer.criterion
-        )
+        specific_criterion = _get_specific_criterion(specific_experiment_criteria, specific_criterion_answer.criterion)
 
-        if specific_criterion and not specific_criterion_answer.answer == \
-                                      specific_criterion.correct_value:
+        if specific_criterion and not specific_criterion_answer.answer == specific_criterion.correct_value:
             return True
 
     return False
 
 
-def _get_specific_criterion(specific_experiment_criteria, criterion) -> \
-        Optional[ExperimentCriterion]:
+def _get_specific_criterion(specific_experiment_criteria, criterion) -> Optional[ExperimentCriterion]:
     """
     Gets the experimentCriterion object for a criterion object
     :param specific_experiment_criteria:
@@ -221,8 +207,9 @@ def should_exclude_by_age(participant: Participant, criteria: DefaultCriteria) -
     if participant.age is None:
         return False
 
-    age_pred = age(months=criteria.min_age_months, days=criteria.min_age_days)\
-        .to(months=criteria.max_age_months, days=criteria.max_age_days)
+    age_pred = age(months=criteria.min_age_months, days=criteria.min_age_days).to(
+        months=criteria.max_age_months, days=criteria.max_age_days
+    )
 
     return not age_pred.check(participant.birth_date)
 
@@ -232,5 +219,4 @@ def should_exclude_by_call_status(participant: Participant, experiment: Experime
     When a parent indicates they cannot participate, there should be a Call object
     for the relevant participant and experiment, with an EXCLUDE status
     """
-    return participant.call_set.filter(
-        experiment=experiment, status=Call.CallStatus.EXCLUDE).exists()
+    return participant.call_set.filter(experiment=experiment, status=Call.CallStatus.EXCLUDE).exists()
