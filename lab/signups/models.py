1be8b58d0c62fd674b3bc6324c444f2ce9701e75
ben <b.bonfil@uu.nl>
2023-01-27 11:05:29 +0100

    updated Signup to use encrypted fields

--- lab/signups/models.py
+++ lab/signups/models.py
@@ -1,28 +1,30 @@
+import cdh.core.fields as e_fields
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 
 class Signup(models.Model):
-    name = models.CharField(max_length=100)
-    sex = models.CharField(max_length=1)
-    birth_date = models.DateField()
+    name = e_fields.EncryptedCharField(max_length=100)
+    sex = e_fields.EncryptedCharField(max_length=1)
+    birth_date = e_fields.EncryptedDateField()
 
-    parent_name = models.TextField()
-    city = models.TextField()
-    phonenumber = models.TextField()
-    phonenumber_alt = models.TextField(blank=True)
-    email = models.TextField()
+    parent_name = e_fields.EncryptedTextField()
+    city = e_fields.EncryptedTextField()
+    phonenumber = e_fields.EncryptedTextField()
+    phonenumber_alt = e_fields.EncryptedTextField(blank=True)
+    email = e_fields.EncryptedTextField()
 
-    english_contact = models.BooleanField()
-    newsletter = models.BooleanField()
+    english_contact = e_fields.EncryptedBooleanField()
+    newsletter = e_fields.EncryptedBooleanField()
 
-    dyslexic_parent = models.BooleanField()
-    tos_parent = models.BooleanField()
-    speech_parent = models.BooleanField()
-    multilingual = models.BooleanField()
+    dyslexic_parent = e_fields.EncryptedBooleanField()
+    tos_parent = e_fields.EncryptedBooleanField()
+    speech_parent = e_fields.EncryptedBooleanField()
+    multilingual = e_fields.EncryptedBooleanField()
 
     class Status(models.TextChoices):
-        NEW = 'NEW', _('signups:stats:new')
-        APPROVED = 'APPROVED', _('signups:stats:approved')
-        REJECTED = 'REJECTED', _('signups:stats:rejected')
-    status = models.CharField(max_length=20, choices=Status.choices, default=Status.NEW)
+        NEW = "NEW", _("signups:stats:new")
+        APPROVED = "APPROVED", _("signups:stats:approved")
+        REJECTED = "REJECTED", _("signups:stats:rejected")
+
+    status = e_fields.EncryptedTextField(max_length=20, choices=Status.choices, default=Status.NEW)
