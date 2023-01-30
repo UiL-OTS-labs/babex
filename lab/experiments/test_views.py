f350052a73eaa11353982131778406c05e3b1497
Ben Bonfil <b.bonfil@uu.nl>
2023-01-30 11:14:42 +0100

    Allow calling participants two weeks earlier than matching age range (#47)
    * added freezegun to requirements
    
    * allow participants who will be of the correct age within 14 days
    
    * black

--- lab/experiments/test_views.py
+++ lab/experiments/test_views.py
@@ -1,8 +1,10 @@
 import json
-from datetime import timedelta
+from datetime import date, timedelta
 
+from django.core.exceptions import BadRequest
 from django.test import TestCase
 from django.utils import timezone
+from freezegun import freeze_time
 from rest_framework.test import APIRequestFactory, force_authenticate
 
 from main.models import User
@@ -10,12 +12,13 @@ from participants.models import Participant
 
 from .models import Experiment
 from .views.call_views import AppointmentConfirm
+from .views.invite_views import InviteParticipantsForExperimentView
 
 
 class AppointmentTests(TestCase):
     @classmethod
     def setUpTestData(cls):
-        cls.user = User.objects.create(username='test', is_staff=True)
+        cls.user = User.objects.create(username="test", is_staff=True)
         cls.experiment = Experiment.objects.create()
         cls.experiment.leaders.add(cls.user)
 
@@ -28,32 +31,32 @@ class AppointmentTests(TestCase):
 
     def test_appointment_confirm(self):
         data = {
-            'experiment': self.experiment.pk,
-            'start': timezone.now() + timedelta(days=1),
-            'end': timezone.now() + timedelta(days=1, hours=1),
-            'leader': self.user.pk,
-            'participant': self.participant.pk,
-            'emailParticipant': False
+            "experiment": self.experiment.pk,
+            "start": timezone.now() + timedelta(days=1),
+            "end": timezone.now() + timedelta(days=1, hours=1),
+            "leader": self.user.pk,
+            "participant": self.participant.pk,
+            "emailParticipant": False,
         }
 
-        request = self.factory.post('/experiments/call/appointment/', data, format='json')
+        request = self.factory.post("/experiments/call/appointment/", data, format="json")
         force_authenticate(request, self.user)
         response = AppointmentConfirm.as_view()(request)
         self.assert_(json.loads(response.content))
 
     def test_appointment_confirm_fail_in_past(self):
         data = {
-            'experiment': self.experiment.pk,
-            'start': timezone.now() - timedelta(hours=2),
-            'end': timezone.now() - timedelta(hours=1),
-            'leader': self.user.pk,
-            'participant': self.participant.pk,
-            'emailParticipant': False
+            "experiment": self.experiment.pk,
+            "start": timezone.now() - timedelta(hours=2),
+            "end": timezone.now() - timedelta(hours=1),
+            "leader": self.user.pk,
+            "participant": self.participant.pk,
+            "emailParticipant": False,
         }
 
-        request = self.factory.post('/experiments/call/appointment/', data, format='json')
+        request = self.factory.post("/experiments/call/appointment/", data, format="json")
         force_authenticate(request, self.user)
-        with self.assertRaises(Exception):
+        with self.assertRaises(BadRequest):
             AppointmentConfirm.as_view()(request)
 
     def test_appointment_confirm_when_eligible_age(self):
@@ -66,15 +69,15 @@ class AppointmentTests(TestCase):
         self.participant.save()
 
         data = {
-            'experiment': self.experiment.pk,
-            'start': timezone.now() + timedelta(days=7),
-            'end': timezone.now() + timedelta(days=7, hours=1),
-            'leader': self.user.pk,
-            'participant': self.participant.pk,
-            'emailParticipant': False
+            "experiment": self.experiment.pk,
+            "start": timezone.now() + timedelta(days=7),
+            "end": timezone.now() + timedelta(days=7, hours=1),
+            "leader": self.user.pk,
+            "participant": self.participant.pk,
+            "emailParticipant": False,
         }
 
-        request = self.factory.post('/experiments/call/appointment/', data, format='json')
+        request = self.factory.post("/experiments/call/appointment/", data, format="json")
         force_authenticate(request, self.user)
         AppointmentConfirm.as_view()(request)
 
@@ -88,15 +91,69 @@ class AppointmentTests(TestCase):
         self.participant.save()
 
         data = {
-            'experiment': self.experiment.pk,
-            'start': timezone.now() + timedelta(days=7),
-            'end': timezone.now() + timedelta(days=7, hours=1),
-            'leader': self.user.pk,
-            'participant': self.participant.pk,
-            'emailParticipant': False
+            "experiment": self.experiment.pk,
+            "start": timezone.now() + timedelta(days=7),
+            "end": timezone.now() + timedelta(days=7, hours=1),
+            "leader": self.user.pk,
+            "participant": self.participant.pk,
+            "emailParticipant": False,
         }
 
-        request = self.factory.post('/experiments/call/appointment/', data, format='json')
+        request = self.factory.post("/experiments/call/appointment/", data, format="json")
         force_authenticate(request, self.user)
         with self.assertRaises(Exception):
             AppointmentConfirm.as_view()(request)
+
+
+class InviteTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = User.objects.create(username="test", is_staff=True)
+        cls.experiment = Experiment.objects.create()
+        cls.experiment.leaders.add(cls.user)
+
+        cls.participant = Participant.objects.create(
+            dyslexic_parent=False,  # TODO: this is not nullable at the moment of writing the test
+            email_subscription=True,  # TODO: this shouldn't have an effect on exclusions
+            language="nl",  # default experiment language
+        )
+
+        cls.experiment.defaultcriteria.min_age_days = 0
+        cls.experiment.defaultcriteria.min_age_months = 2
+        cls.experiment.defaultcriteria.save()
+
+    def setUp(self):
+        self.factory = APIRequestFactory()
+
+    @freeze_time("2021-03-01")
+    def test_can_invite_participant_correct_min_age(self):
+        self.participant.birth_date = date(2021, 1, 1)
+        self.participant.save()
+
+        request = self.factory.get(f"/experiments/{self.experiment.pk}/invite/")
+        request.user = self.user
+        response = InviteParticipantsForExperimentView.as_view()(request, experiment=self.experiment.pk)
+
+        self.assertEqual(len(response.context_data["object_list"]), 1)
+
+    @freeze_time("2021-03-01")
+    def test_can_invite_participant_within_two_weeks(self):
+        self.participant.birth_date = date(2021, 1, 14)
+        self.participant.save()
+
+        request = self.factory.get(f"/experiments/{self.experiment.pk}/invite/")
+        request.user = self.user
+        response = InviteParticipantsForExperimentView.as_view()(request, experiment=self.experiment.pk)
+
+        self.assertEqual(len(response.context_data["object_list"]), 1)
+
+    @freeze_time("2021-03-01")
+    def test_cannot_invite_participant_when_too_young(self):
+        self.participant.birth_date = date(2021, 1, 20)
+        self.participant.save()
+
+        request = self.factory.get(f"/experiments/{self.experiment.pk}/invite/")
+        request.user = self.user
+        response = InviteParticipantsForExperimentView.as_view()(request, experiment=self.experiment.pk)
+
+        self.assertEqual(len(response.context_data["object_list"]), 0)
