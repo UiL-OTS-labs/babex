4b8745ac24038377bf55a9ee8b666cc0fc38d798
ben <b.bonfil@uu.nl>
2023-04-21 15:14:56 +0200

    fix experiment creation form

--- lab/experiments/forms/experiment_forms.py
+++ lab/experiments/forms/experiment_forms.py
@@ -1,9 +1,12 @@
+from cdh.core.forms import (
+    BootstrapCheckboxInput,
+    BootstrapRadioSelect,
+    TemplatedModelForm,
+)
+from cdh.core.mail.widgets import EmailContentEditWidget
 from django import forms
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
-
-from cdh.core.forms import TemplatedModelForm, BootstrapCheckboxInput, BootstrapRadioSelect
-from cdh.core.mail.widgets import EmailContentEditWidget
 
 from ..models import Experiment
 
@@ -11,49 +14,54 @@ from ..models import Experiment
 class ExperimentForm(TemplatedModelForm):
     class Meta:
         model = Experiment
-        fields = '__all__'
+        fields = "__all__"
+        exclude = ("defaultcriteria",)
         widgets = {
-            'name':         forms.TextInput,
-            'duration':     forms.TextInput,
-            'compensation': forms.TextInput,
-            'use_timeslots': BootstrapRadioSelect(choices=(
-                (True, _("experiment:form:use_timeslots:true")),
-                (False, _("experiment:form:use_timeslots:false")),
-            )),
-            'task_description': forms.Textarea({
-                'rows': 7,
-            }),
-            'additional_instructions': forms.Textarea({
-                'rows': 7
-            }),
-            'confirmation_email': EmailContentEditWidget(None),
-            'invite_email': EmailContentEditWidget(None),
-            'open': BootstrapCheckboxInput,
-            'public': BootstrapCheckboxInput,
+            "name": forms.TextInput,
+            "duration": forms.TextInput,
+            "compensation": forms.TextInput,
+            "use_timeslots": BootstrapRadioSelect(
+                choices=(
+                    (True, _("experiment:form:use_timeslots:true")),
+                    (False, _("experiment:form:use_timeslots:false")),
+                )
+            ),
+            "task_description": forms.Textarea(
+                {
+                    "rows": 7,
+                }
+            ),
+            "additional_instructions": forms.Textarea({"rows": 7}),
+            "confirmation_email": EmailContentEditWidget(None),
+            "invite_email": EmailContentEditWidget(None),
+            "open": BootstrapCheckboxInput,
+            "public": BootstrapCheckboxInput,
         }
 
     def __init__(self, *args, **kwargs):
         super(ExperimentForm, self).__init__(*args, **kwargs)
 
-        self.fields['default_max_places'].widget.attrs.update(
+        self.fields["default_max_places"].widget.attrs.update(
             {
-                'min': 1,
+                "min": 1,
             }
         )
 
-        self.fields['confirmation_email'].widget.preview_url = self.preview_url_confirmation()
-        self.fields['invite_email'].widget.preview_url = self.preview_url_invite()
+        self.fields["confirmation_email"].widget.preview_url = self.preview_url_confirmation()
+        self.fields["invite_email"].widget.preview_url = self.preview_url_invite()
 
         # If we are updating an experiment, make sure you cannot exclude the
         # experiment you are updating!
         if self.instance:
             other_experiments = Experiment.objects.exclude(pk=self.instance.pk)
-            self.fields['excluded_experiments'].choices = [
-                (x.pk, x.name) for x in other_experiments
-            ]
+            self.fields["excluded_experiments"].choices = [(x.pk, x.name) for x in other_experiments]
 
     def preview_url_confirmation(self):
-        return reverse('experiments:email_preview', args=('confirmation', self.instance.pk))
+        if self.instance.pk is not None:
+            return reverse("experiments:email_preview", args=("confirmation", self.instance.pk))
+        # TODO: how to preview with unsaved experiment?
 
     def preview_url_invite(self):
-        return reverse('experiments:email_preview', args=('invite', self.instance.pk))
+        if self.instance.pk is not None:
+            return reverse("experiments:email_preview", args=("invite", self.instance.pk))
+        # TODO
