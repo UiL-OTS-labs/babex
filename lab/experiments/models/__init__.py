5114eb0a50e7e6a140015947507bd037902a6a5e
ben <b.bonfil@uu.nl>
2023-04-24 14:08:10 +0200

    make_appointment() helper method

--- lab/experiments/models/__init__.py
+++ lab/experiments/models/__init__.py
@@ -1,6 +1,6 @@
+from .appointment_models import Appointment, TimeSlot, make_appointment
 from .criteria_models import Criterion, ExperimentCriterion
 from .default_criteria_models import DefaultCriteria
 from .experiment_models import Experiment
 from .invite_models import Invitation
 from .location_models import Location
-from .appointment_models import Appointment, TimeSlot
