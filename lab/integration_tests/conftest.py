9d4889558df9cfbc2f24b8a3ff7a41805fcb1ef0
ben <b.bonfil@uu.nl>
2023-06-12 16:38:16 +0200

    support for required experiments

--- lab/integration_tests/conftest.py
+++ lab/integration_tests/conftest.py
@@ -1,20 +1,4 @@
-from datetime import date, datetime
-
 import pytest
-
-from experiments.models import Appointment, DefaultCriteria, TimeSlot
-from main.models import User
-from participants.models import Participant
-
-
-@pytest.fixture
-def admin_user(db):
-    admin = User.objects.create(
-        username="admin", is_superuser=True, is_staff=True, name="Admin McAdmin", phonenumber="12345678"
-    )
-    admin.set_password("admin")
-    admin.save()
-    yield admin
 
 
 @pytest.fixture
@@ -37,29 +21,3 @@ def as_leader(sb, django_user_model, live_server):
     sb.type("#id_password", password)
     sb.click('button:contains("Log in")')
     yield user
-
-
-@pytest.fixture
-def sample_experiment(admin_user, db):
-    yield admin_user.experiments.create(defaultcriteria=DefaultCriteria.objects.create())
-
-
-@pytest.fixture
-def sample_participant(db):
-    yield Participant.objects.create(
-        email="baby@baby.com",
-        name="Baby McBaby",
-        parent_name="Parent McParent",
-        birth_date=date(2020, 1, 1),
-        multilingual=False,
-        phonenumber="987654321",
-        dyslexic_parent=False,
-        language="nl",
-        capable=True,
-        email_subscription=True,
-    )
-
-
-@pytest.fixture
-def sample_leader(db):
-    yield User.objects.create(name="Leader McLeader", username="leader", phonenumber="23456789")
