dd16f287df63f42bcb5c25fef8929debec2993a5
ben <b.bonfil@uu.nl>
2023-06-13 10:06:11 +0200

    resolved issues with fixtures

--- lab/conftest.py
+++ lab/conftest.py
@@ -8,16 +8,6 @@ from participants.models import Participant
 
 
 @pytest.fixture
-def admin_user(db):
-    admin = User.objects.create(
-        username="admin", is_superuser=True, is_staff=True, name="Admin McAdmin", phonenumber="12345678"
-    )
-    admin.set_password("admin")
-    admin.save()
-    yield admin
-
-
-@pytest.fixture
 def sample_experiment(admin_user, db):
     yield admin_user.experiments.create(defaultcriteria=DefaultCriteria.objects.create())
 
--- lab/integration_tests/conftest.py
+++ lab/integration_tests/conftest.py
@@ -1,12 +1,22 @@
+from datetime import date
+
 import pytest
+
+from experiments.models import DefaultCriteria
+from main.models import User
+from participants.models import Participant
 
 
 @pytest.fixture
 def as_admin(sb, admin_user, live_server):
+    admin_user.name = "Admin McAdmin"
+    admin_user.phonenumber = "12345678"
+    admin_user.save()
+
     sb.open(live_server.url)
     # sb.click('#djHideToolBarButton')
     sb.type("#id_username", "admin")
-    sb.type("#id_password", "admin")
+    sb.type("#id_password", "password")
     sb.click('button:contains("Log in")')
 
 
@@ -21,3 +31,29 @@ def as_leader(sb, django_user_model, live_server):
     sb.type("#id_password", password)
     sb.click('button:contains("Log in")')
     yield user
+
+
+@pytest.fixture
+def sample_experiment(admin_user, db):
+    yield admin_user.experiments.create(defaultcriteria=DefaultCriteria.objects.create())
+
+
+@pytest.fixture
+def sample_participant(db):
+    yield Participant.objects.create(
+        email="baby@baby.com",
+        name="Baby McBaby",
+        parent_name="Parent McParent",
+        birth_date=date(2020, 1, 1),
+        multilingual=False,
+        phonenumber="987654321",
+        dyslexic_parent=False,
+        language="nl",
+        capable=True,
+        email_subscription=True,
+    )
+
+
+@pytest.fixture
+def sample_leader(db):
+    yield User.objects.create(name="Leader McLeader", username="leader", phonenumber="23456789")
