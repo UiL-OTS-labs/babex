3c322dce15b1b008d6c145953cd7649dfcc808ac
ben <b.bonfil@uu.nl>
2023-06-20 15:28:06 +0200

    cleanup of unused views

--- lab/experiments/urls/__init__.py
+++ lab/experiments/urls/__init__.py
@@ -3,34 +3,19 @@ from django.urls import path
 
 from ..views.email_views import email_preview
 
-app_name = 'experiments'
+app_name = "experiments"
 
 urlpatterns = [
-    path('', include('experiments.urls.experiments_urls')),
-
+    path("", include("experiments.urls.experiments_urls")),
     # Participant in experiment view
-    path('<int:experiment>/', include('experiments.urls.experiments_urls.participants')),
-
-    # Excluded experiments views
-    path('', include('experiments.urls.experiments_urls.excluded')),
-
-    # Experiment timeslot views
-    path('', include('experiments.urls.appointment_urls')),
-
+    path("<int:experiment>/", include("experiments.urls.experiments_urls.participants")),
     # Experiment related criteria views
-    path('<int:experiment>/', include('experiments.urls.criteria_urls.experiment')),
-
-    # Experiment state switchers
-    path('<int:pk>/', include('experiments.urls.experiments_urls.state_switchers')),
-
+    path("<int:experiment>/", include("experiments.urls.criteria_urls.experiment")),
     # Stand-alone location views
-    path('locations/', include('experiments.urls.locations_urls')),
-
+    path("locations/", include("experiments.urls.locations_urls")),
     # Stand-alone criteria views
-    path('criteria/', include('experiments.urls.criteria_urls.standalone')),
-
-    path('', include('experiments.urls.call_urls')),
-
+    path("criteria/", include("experiments.urls.criteria_urls.standalone")),
+    path("", include("experiments.urls.call_urls")),
     # email preview
-    path('email/preview/<str:template>/<int:experiment>/', email_preview, name='email_preview'),
+    path("email/preview/<str:template>/<int:experiment>/", email_preview, name="email_preview"),
 ]
