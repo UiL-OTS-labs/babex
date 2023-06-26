d5c9cf6089d934d5bd1bc9510273fd76851f2129
Ben Bonfil <b.bonfil@uu.nl>
2023-06-26 11:39:28 +0200

    Feature/outcome reminder (#87)
    * limit location editing to superadmin
    
    * removed datamanagement app for now
    
    * cleanup of unused views
    
    * replaced all instances of LoginRequiredMixin (except home) with finer
    grained leader permission checks
    
    * replaced all uses of IsAuthenticated
    
    * changed agenda home view to class-based view, for permission mixins
    
    * fixed tests
    
    * fixed parent Makefile
    
    * removing irrelevant timeslot related code
    
    * remind leaders to set the outcome of past appointments
    
    * fixed url mapping
    
    * fixed broken month view
    
    * merge problem

--- lab/main/views/home.py
+++ lab/main/views/home.py
@@ -1,18 +1,27 @@
 import braces.views as braces
+from django.utils import timezone
 from django.views import generic
 
 from experiments.models.invite_models import Call
 
 
 class HomeView(braces.LoginRequiredMixin, generic.TemplateView):
-    template_name = 'main/index.html'
+    template_name = "main/index.html"
 
     def get_context_data(self, *args, **kwargs):
         context = super().get_context_data(**kwargs)
 
         user = self.request.user
-        context['experiments'] = user.experiments.all()
-        context['call_back'] = set(call.participant for call in
-                                   user.call_set.filter(status=Call.CallStatus.CALLBACK))
+
+        if self.request.user.is_leader:
+            context["experiments"] = user.experiments.all()
+            context["call_back"] = set(
+                call.participant for call in user.call_set.filter(status=Call.CallStatus.CALLBACK)
+            )
+            context["missing_outcome"] = (
+                user.appointment_set.filter(outcome=None)
+                .filter(timeslot__start__lte=timezone.now())
+                .order_by("-timeslot__start")
+            )
 
         return context
