c466a66dc3c638602d8eaa70a37d3b346dddae5b
ben <b.bonfil@uu.nl>
2023-06-27 11:06:36 +0200

    collect.py for preparing messages json files

---
+++ babex-vue/collect.py
@@ -0,0 +1,46 @@
+"""This script collects translatable messages from .vue and .ts files
+based on calls to the translation function _()
+It creates two json files containing en and nl messages in the src folders
+"""
+import re
+import json
+from pathlib import Path
+from itertools import chain
+from functools import partial
+
+
+def collect_messages(language, output_path):
+    try:
+        existing = json.load(open(output_path))
+    except FileNotFoundError:
+        existing = {}
+
+    i18n_re = re.compile(r'''_\(['"](.+?)['"]\)''')
+
+    with open(output_path, 'w') as out:
+        p = partial(print, file=out)
+        p('{\n')
+        for f in chain(Path('src').rglob('*.vue'), Path('src').rglob('*.ts')):
+            for idx, line in enumerate(open(f)):
+                results = i18n_re.findall(line)
+                for result in results:
+                    p(f'"//": "{f}:{idx}",')
+                    m = existing.get(result)
+                    if m is None:
+                        p(f'"{result}": null,')
+                    else:
+                        p(f'"{result}"', '"{}",'.format(m), sep=': ')
+
+        # empty 'comment' field to deal with trailing comma
+        p('\n"//": ""')
+        p('}')
+
+
+def main():
+    target = 'src/messages.{}.json'
+    collect_messages('en', target.format('en'))
+    collect_messages('nl', target.format('nl'))
+
+
+if __name__ == '__main__':
+    main()
