1dbd7d620a3883f2b0b7aa805a5a1796547771b7
ben <b.bonfil@uu.nl>
2023-06-27 11:54:16 +0200

    cleanup of fuzzy translations

--- lab/experiments/email.py
+++ lab/experiments/email.py
@@ -1,19 +1,21 @@
-from django.utils.translation import gettext_lazy as _
-
 from cdh.core.mail import BaseCustomTemplateEmail, CTEVarDef
+from django.utils.translation import gettext_lazy as _
 
 
 class AppointmentConfirmEmail(BaseCustomTemplateEmail):
     user_variable_defs = [
-        CTEVarDef(name, _(f'experiments.email.appointmentconfirm.var.{name}'))
-        for name in [
-            'baby', 'experiment_name', 'date', 'time', 'experiment_location',
-            'leader_name', 'leader_email', 'leader_phonenumber'
-        ]
+        CTEVarDef("baby", _("experiments.email.appointmentconfirm.var.baby")),
+        CTEVarDef("experiment_name", _("experiments.email.appointmentconfirm.var.experiment_name")),
+        CTEVarDef("date", _("experiments.email.appointmentconfirm.var.date")),
+        CTEVarDef("time", _("experiments.email.appointmentconfirm.var.time")),
+        CTEVarDef("experiment_location", _("experiments.email.appointmentconfirm.var.experiment_location")),
+        CTEVarDef("leader_name", _("experiments.email.appointmentconfirm.var.leader_name")),
+        CTEVarDef("leader_email", _("experiments.email.appointmentconfirm.var.leader_email")),
+        CTEVarDef("leader_phonenumber", _("experiments.email.appointmentconfirm.var.leader_phonenumber")),
     ]
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
         self.banner = self.subject
-        self.language = 'nl'
+        self.language = "nl"
