5faac5fd7cd1d05a3d399232bbd4b8a3451f7791
Ben <b.bonfil@uu.nl>
2023-11-13 14:55:48 +0100

    updated integration tests settings to use settings_base

--- integration_tests/lab_settings.py
+++ integration_tests/lab_settings.py
@@ -1,13 +1,25 @@
 import sys
+import base64
 sys.path.append('../lab')
 
-from ppn_backend.settings import *
+from ppn_backend.settings_base import *
 
+SECRET_KEY = "something else"
+FIELD_ENCRYPTION_KEY = base64.b64encode(b"a" * 32)
 PARENT_URI = 'http://localhost:19000/'
 
+EMAIL_FROM = "babex@localhost.local"
 EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
 EMAIL_FILE_PATH = '/tmp/babex-mailbox'
 
-DATABASES['default']['NAME'] = 'lab.int.db.sqlite3'
+DATABASES = {
+    'default': {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": 'lab.int.db.sqlite3',
+    }
+}
 
 FIXTURE_DIRS = ['data_fixtures']
+
+ALLOWED_HOSTS = ["localhost"]
+DEBUG = True
