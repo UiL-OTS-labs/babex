3988852cb2101d015a6ed92d20b4429a68312a50
Ben Bonfil <b.bonfil@uu.nl>
2023-12-05 11:05:18 +0100

    allow saving extra data on participant profiles (for test results etc.) (#133)

--- lab/participants/models.py
+++ lab/participants/models.py
@@ -160,3 +160,10 @@ class CriterionAnswer(models.Model):
 
     def __str__(self):
         return "({}) {}: {}".format(self.participant.name, self.criterion.name_natural, self.answer)
+
+
+class ExtraData(models.Model):
+    participant = models.ForeignKey(Participant, on_delete=models.CASCADE)
+    title = e_fields.EncryptedCharField(max_length=255, verbose_name=_("extradata:attribute:title"))
+    created = e_fields.EncryptedDateTimeField(verbose_name=_("extradata:attribute:created"), auto_now_add=True)
+    content = e_fields.EncryptedTextField()
