110c3568f3ca5a60e6e98d6b1abb3a31bc117428
Ben Bonfil <b.bonfil@uu.nl>
2024-02-12 16:47:45 +0100

    send another mail to the participant whenever on appointment update (#185)
    * send another mail to the participant whenever on appointment update
    
    * wasn't checking timeslot end correctly
    
    * updated send_appointment_mail call
    
    * fixed build script

--- lab/integration_tests/test_agenda.py
+++ lab/integration_tests/test_agenda.py
@@ -1,9 +1,11 @@
 from datetime import date, datetime, timedelta
 
 import pytest
+from django.core import mail
+from django.utils import timezone
 
 from agenda.models import Closing
-from experiments.models import Appointment, DefaultCriteria, TimeSlot
+from experiments.models import Appointment, TimeSlot
 
 
 @pytest.fixture
@@ -44,10 +46,27 @@ def test_agenda_edit_closing(sb, sample_closing, as_admin, agenda):
 
 @pytest.fixture
 def appointment_yesterday(db, sample_experiment, sample_leader, sample_participant):
-    midnight = datetime(date.today().year, date.today().month, date.today().day, 0, 0)
+    midnight = datetime(
+        date.today().year, date.today().month, date.today().day, 0, 0, tzinfo=timezone.get_current_timezone()
+    )
     timeslot = TimeSlot.objects.create(
         start=midnight - timedelta(hours=13),
         end=midnight - timedelta(hours=12),
+        experiment=sample_experiment,
+    )
+    yield Appointment.objects.create(
+        participant=sample_participant, experiment=sample_experiment, timeslot=timeslot, leader=sample_leader
+    )
+
+
+@pytest.fixture
+def appointment_tomorrow(db, sample_experiment, sample_leader, sample_participant):
+    start = datetime(
+        date.today().year, date.today().month, date.today().day, 12, 0, tzinfo=timezone.get_current_timezone()
+    ) + timedelta(days=1)
+    timeslot = TimeSlot.objects.create(
+        start=start,
+        end=start + timedelta(hours=1),
         experiment=sample_experiment,
     )
     yield Appointment.objects.create(
@@ -83,3 +102,24 @@ def test_agenda_set_appointment_outcome(sb, appointment_yesterday, as_leader):
     sb.click("button.icon-menu")
     sb.click("a:contains(Invite)")
     sb.assert_text_visible(appointment_yesterday.participant.name)
+
+
+def test_agenda_modify_appointment(sb, appointment_tomorrow, as_leader):
+    appointment_tomorrow.experiment.leaders.add(as_leader)
+    sb.click('a:contains("Agenda")')
+
+    sb.click(f'td[data-date="{appointment_tomorrow.start.date()}"]')
+    original_time = appointment_tomorrow.timeslot.start
+    new_time = original_time + timedelta(days=3)
+
+    sb.type(".appointment-start input", new_time.strftime("%d-%m-%Y %H:%M"))
+    sb.type(".appointment-end input", (new_time + timedelta(hours=1)).strftime("%d-%m-%Y %H:%M"))
+    sb.click(".action-panel .save")
+
+    sb.assert_element_not_visible(".action-panel .save")
+    appointment_tomorrow.refresh_from_db()
+    assert appointment_tomorrow.timeslot.start == new_time
+
+    # check that an appointment update email was sent
+    sb.assertEqual(len(mail.outbox), 1)
+    sb.assertEqual(mail.outbox[0].to[0], appointment_tomorrow.participant.email)
