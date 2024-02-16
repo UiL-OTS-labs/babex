9d0034468c340ed3bd64bed285d64d97d1042431
bbonf <b.bonfil@uu.nl>
2024-02-16 10:19:08 +0100

    no longer necessary as footer is explicitly enabled for all emails

--- lab/experiments/email.py
+++ lab/experiments/email.py
@@ -1,6 +1,5 @@
 from cdh.core.mail import BaseCustomTemplateEmail, CTEVarDef
 from django.utils.translation import gettext_lazy as _
-from django.template.loader import render_to_string
 
 
 class AppointmentConfirmEmail(BaseCustomTemplateEmail):
@@ -20,4 +19,3 @@ class AppointmentConfirmEmail(BaseCustomTemplateEmail):
 
         self.banner = self.subject
         self.language = "nl"
-        self.footer = render_to_string("mail/footer.html")
