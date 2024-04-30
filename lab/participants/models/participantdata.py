7cbf3f011338ecca02fd73944d478fb866b6ace4
bbonf <b.bonfil@uu.nl>
2024-04-30 15:55:36 +0200

    removed null=True from ParticipantData fields

--- lab/participants/models/participantdata.py
+++ lab/participants/models/participantdata.py
@@ -11,25 +11,24 @@ from .language import Language
 class ParticipantData(models.Model):
     objects = EncryptedManager()
 
-    name = e_fields.EncryptedTextField(_("participant:attribute:name"), blank=True, null=True)
-    sex = e_fields.EncryptedTextField(_("participant:attribute:sex"), null=True, choices=Sex.choices)
-    birth_date = e_fields.EncryptedDateField(_("participant:attribute:birth_date"), blank=True, null=True)
+    name = e_fields.EncryptedTextField(_("participant:attribute:name"), blank=True)
+    sex = e_fields.EncryptedTextField(_("participant:attribute:sex"), choices=Sex.choices)
+    birth_date = e_fields.EncryptedDateField(_("participant:attribute:birth_date"), blank=True)
 
     birth_weight = e_fields.EncryptedCharField(
-        _("participant:attribute:birth_weight"), null=True, choices=BirthWeight.choices, max_length=30
+        _("participant:attribute:birth_weight"), choices=BirthWeight.choices, max_length=30
     )
     pregnancy_duration = e_fields.EncryptedCharField(
         _("participant:attribute:pregnancy_duration"),
-        null=True,
         choices=PregnancyDuration.choices,
         max_length=30,
     )
     languages = models.ManyToManyField(Language)
 
-    parent_first_name = e_fields.EncryptedTextField(_("participant:attribute:parent_first_name"), null=True)
-    parent_last_name = e_fields.EncryptedTextField(_("participant:attribute:parent_last_name"), null=True)
+    parent_first_name = e_fields.EncryptedTextField(_("participant:attribute:parent_first_name"))
+    parent_last_name = e_fields.EncryptedTextField(_("participant:attribute:parent_last_name"))
     email = e_fields.EncryptedEmailField(_("participant:attribute:email"))
-    phonenumber = e_fields.EncryptedTextField(_("participant:attribute:phonenumber"), blank=True, null=True)
+    phonenumber = e_fields.EncryptedTextField(_("participant:attribute:phonenumber"), blank=True)
     phonenumber_alt = e_fields.EncryptedTextField(_("participant:attribute:phonenumber_alt"), blank=True, null=True)
 
     dyslexic_parent = e_fields.EncryptedCharField(
