43a904111cd281b49035555821fb0025873ba028
bbonf <b.bonfil@uu.nl>
2024-09-20 11:23:57 +0200

    fixup! improper use of async and ApiRequest caused issues with saving

--- babex-vue/src/components/invite/CallHome.vue
+++ babex-vue/src/components/invite/CallHome.vue
@@ -136,10 +136,6 @@
             complete();
         });
     }
-            saving.value = false;
-            complete();
-        });
-    }
 
     function complete() {
         location.href = props.completeUrl;
