45b7afcc4b9aad9351e3a6c146b33720bd51558f
bbonf <b.bonfil@uu.nl>
2024-10-01 14:31:10 +0200

    fix query for appointment reminders (closes #229)

--- lab/experiments/management/commands/send_reminders.py
+++ lab/experiments/management/commands/send_reminders.py
@@ -14,11 +14,12 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         # fetch appoinments 24 hours from now
         now = datetime.now()
-        threshold = now + timedelta(days=1)
+        threshold_from = now + timedelta(days=1)
+        threshold_to = now + timedelta(days=2)
 
         appointments = Appointment.objects.filter(
-            timeslot__start__gt=now, timeslot__start__lte=threshold, reminder_sent=None
-        )
+            timeslot__start__gt=threshold_from, timeslot__start__lte=threshold_to, reminder_sent=None
+        ).exclude(outcome=Appointment.Outcome.CANCELED)
 
         for appointment in appointments:
             mail = TemplateEmail(
