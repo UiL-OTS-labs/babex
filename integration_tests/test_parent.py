462ec857c0bd6aabce718d13ebbc434d5024b3e7
bbonf <b.bonfil@uu.nl>
2025-01-15 14:26:09 +0100

    mail order in test isn't deterministic

--- integration_tests/test_parent.py
+++ integration_tests/test_parent.py
@@ -11,60 +11,63 @@ from playwright.sync_api import Page, expect
 @pytest.fixture
 def default_signup_fill_form(page: Page, apps):
     """opens the signup form and fills some default values. individual tests should overwrite as necessary"""
-    page.goto(apps.parent.url + 'signup/')
+    page.goto(apps.parent.url + "signup/")
 
-    page.fill('#id_name', 'Test Baby')
-    page.click('#id_sex_0')
+    page.fill("#id_name", "Test Baby")
+    page.click("#id_sex_0")
 
-    page.locator('#id_birth_date_month').select_option('March')
-    page.locator('#id_birth_date_day').select_option('5')
-    page.locator('#id_birth_date_year').select_option(str(datetime.datetime.now().year - 1))
+    page.locator("#id_birth_date_month").select_option("March")
+    page.locator("#id_birth_date_day").select_option("5")
+    page.locator("#id_birth_date_year").select_option(
+        str(datetime.datetime.now().year - 1)
+    )
 
-    page.click('#id_birth_weight_1')
-    page.click('#id_pregnancy_duration_1')
-    page.click('#id_dyslexic_parent_3')
-    page.click('#id_tos_parent_0')
-    page.click('#id_languages_mono_dutch')
+    page.click("#id_birth_weight_1")
+    page.click("#id_pregnancy_duration_1")
+    page.click("#id_dyslexic_parent_3")
+    page.click("#id_tos_parent_0")
+    page.click("#id_languages_mono_dutch")
 
-    page.fill('#id_parent_first_name', 'Test')
-    page.fill('#id_parent_last_name', 'Parent')
-    page.fill('#id_phonenumber', '06412345678')
-    page.fill('#id_phonenumber_alt', '06487654321')
+    page.fill("#id_parent_first_name", "Test")
+    page.fill("#id_parent_last_name", "Parent")
+    page.fill("#id_phonenumber", "06412345678")
+    page.fill("#id_phonenumber_alt", "06487654321")
 
-    page.fill('#id_email', 'parent@example.com')
-    page.fill('#id_email_again', 'parent@example.com')
+    page.fill("#id_email", "parent@example.com")
+    page.fill("#id_email_again", "parent@example.com")
 
-    page.click('#id_save_longer_0')
-    page.click('#id_english_contact_0')
-    page.click('#id_newsletter_0')
-    page.click('#id_data_consent_0')
+    page.click("#id_save_longer_0")
+    page.click("#id_english_contact_0")
+    page.click("#id_newsletter_0")
+    page.click("#id_data_consent_0")
 
 
 @pytest.fixture
 def signup(page: Page, apps, default_signup_fill_form):
     def delegate():
-        suffix = ''.join(random.choice(string.digits) for i in range(4))
-        email = f'parent{suffix}@localhost.local'
+        suffix = "".join(random.choice(string.digits) for i in range(4))
+        email = f"parent{suffix}@localhost.local"
 
-        page.fill('#id_email', email)
-        page.fill('#id_email_again', email)
+        page.fill("#id_email", email)
+        page.fill("#id_email_again", email)
         page.click('input[type="submit"]')
 
         # check that the form was submitted
         expect(page.locator('input[type="submit"]')).not_to_be_visible()
-        assert 'signup/done' in page.url
+        assert "signup/done" in page.url
 
         return email
+
     return delegate
 
 
 def test_parent_login(page, apps, signup, as_admin, link_from_mail, login_as):
     # confirm signup email
     address = signup()
-    if link := link_from_mail(address, 'Bevestiging'):
+    if link := link_from_mail(address, "Bevestiging"):
         page.goto(link)
     else:
-        pytest.fail('could not find validation link')
+        pytest.fail("could not find validation link")
 
     # approve signup
     as_admin.get_by_role("button", name="Participants").click()
@@ -76,16 +79,16 @@ def test_parent_login(page, apps, signup, as_admin, link_from_mail, login_as):
     login_as(address)
 
     # check that login worked
-    expect(page.get_by_text('Appointments')).to_be_visible()
+    expect(page.get_by_text("Appointments")).to_be_visible()
 
 
 def test_parent_login_unapproved(signup, apps, page, link_from_mail, login_as):
     # confirm signup email
     address = signup()
-    if link := link_from_mail(address, 'Bevestiging'):
+    if link := link_from_mail(address, "Bevestiging"):
         page.goto(link)
     else:
-        pytest.fail('could not find validation link')
+        pytest.fail("could not find validation link")
 
     # make sure that no login email has arrived
     assert login_as(address) is False
@@ -98,13 +101,16 @@ def test_parent_self_deactivate(page, participant, login_as, mailbox):
     page.get_by_text("Data management").click()
     page.on("dialog", lambda dialog: dialog.accept())
     page.get_by_text("Remove from").click()
-    page.get_by_text('Remove from').wait_for(state="hidden")
+    page.get_by_text("Remove from").wait_for(state="hidden")
 
     participant.refresh_from_db()
     assert participant.deactivated
 
     assert len(mailbox(email)) == 2
-    assert 'bevestiging van uitschrijven' in mailbox(email)[-1]['subject']
+    assert (
+        "bevestiging van uitschrijven" in mailbox(email)[0]["subject"]
+        or "bevestiging van uitschrijven" in mailbox(email)[1]["subject"]
+    )
 
 
 def test_parent_login_deactivated(participant, apps, page, link_from_mail, login_as):
@@ -114,14 +120,14 @@ def test_parent_login_deactivated(participant, apps, page, link_from_mail, login
 
 
 def test_signup_no_english(page, apps, default_signup_fill_form):
-    Signup = apps.lab.get_model('signups', 'Signup')
+    Signup = apps.lab.get_model("signups", "Signup")
 
-    page.click('#id_english_contact_1')
+    page.click("#id_english_contact_1")
     page.click('input[type="submit"]')
 
     # check that the form was submitted
     expect(page.locator('input[type="submit"]')).not_to_be_visible()
-    assert 'signup/done' in page.url
+    assert "signup/done" in page.url
 
     signup = Signup.objects.last()
     # verify the fields were saved correctly
@@ -129,43 +135,45 @@ def test_signup_no_english(page, apps, default_signup_fill_form):
 
 
 def test_signup_monolingual_dutch(page, apps, default_signup_fill_form):
-    Signup = apps.lab.get_model('signups', 'Signup')
+    Signup = apps.lab.get_model("signups", "Signup")
 
-    page.click('#id_languages_mono_dutch')
+    page.click("#id_languages_mono_dutch")
     page.click('input[type="submit"]')
 
     # check that the form was submitted
     expect(page.locator('input[type="submit"]')).not_to_be_visible()
-    assert 'signup/done' in page.url
+    assert "signup/done" in page.url
 
     signup = Signup.objects.last()
     # verify the fields were saved correctly
-    assert signup.languages == ['Nederlands']
+    assert signup.languages == ["Nederlands"]
 
 
-def test_signup_multilingual_with_custom(page, apps, default_signup_fill_form, as_admin):
-    Signup = apps.lab.get_model('signups', 'Signup')
-    Participant = apps.lab.get_model('participants', 'Participant')
+def test_signup_multilingual_with_custom(
+    page, apps, default_signup_fill_form, as_admin
+):
+    Signup = apps.lab.get_model("signups", "Signup")
+    Participant = apps.lab.get_model("participants", "Participant")
 
-    page.click('#id_languages_multi')
+    page.click("#id_languages_multi")
     # select one language that's already on the list of known languages
-    page.locator('#id_languages_multi_select').select_option('Engels')
+    page.locator("#id_languages_multi_select").select_option("Engels")
 
     # type in one new language
-    search_field = 'input.select2-search__field'
+    search_field = "input.select2-search__field"
     page.click(search_field)
-    page.type(search_field, 'Afrikaans')
-    page.keyboard.press('Enter')
+    page.type(search_field, "Afrikaans")
+    page.keyboard.press("Enter")
 
     page.click('input[type="submit"]')
 
     # check that the form was submitted
     expect(page.locator('input[type="submit"]')).not_to_be_visible()
-    assert 'signup/done' in page.url
+    assert "signup/done" in page.url
 
     signup = Signup.objects.last()
     # verify the fields were saved correctly
-    assert set(signup.languages) == set(['Engels', 'Afrikaans'])
+    assert set(signup.languages) == set(["Engels", "Afrikaans"])
 
     # approve signup
     signup.email_verified = timezone.now()
@@ -177,18 +185,18 @@ def test_signup_multilingual_with_custom(page, apps, default_signup_fill_form, a
 
     # check that the languages were saved correctly
     participant = Participant.objects.last()
-    assert participant.languages.values_list('name', flat=True)
+    assert participant.languages.values_list("name", flat=True)
 
 
 def test_signup_save_longer(page, apps, default_signup_fill_form):
-    Signup = apps.lab.get_model('signups', 'Signup')
+    Signup = apps.lab.get_model("signups", "Signup")
 
-    page.click('#id_save_longer_0')
+    page.click("#id_save_longer_0")
     page.click('input[type="submit"]')
 
     # check that the form was submitted
     expect(page.locator('input[type="submit"]')).not_to_be_visible()
-    assert 'signup/done' in page.url
+    assert "signup/done" in page.url
 
     signup = Signup.objects.last()
     # verify the fields were saved correctly
@@ -197,49 +205,49 @@ def test_signup_save_longer(page, apps, default_signup_fill_form):
 
 def test_signup_unborn(page, apps, default_signup_fill_form):
     tomorrow = datetime.datetime.now() + datetime.timedelta(days=1)
-    page.locator('#id_birth_date_year').select_option(str(tomorrow.year))
-    page.locator('#id_birth_date_month').select_option(tomorrow.strftime('%B'))
-    page.locator('#id_birth_date_day').select_option(str(tomorrow.day))
+    page.locator("#id_birth_date_year").select_option(str(tomorrow.year))
+    page.locator("#id_birth_date_month").select_option(tomorrow.strftime("%B"))
+    page.locator("#id_birth_date_day").select_option(str(tomorrow.day))
 
     # should immediately show error
-    expect(page.locator('select.is-invalid')).to_have_count(3)
+    expect(page.locator("select.is-invalid")).to_have_count(3)
 
 
 def test_signup_confirmation_expired(page, apps, signup, as_admin, link_from_mail):
     # confirm signup email
     address = signup()
 
-    Signup = apps.lab.get_model('signups', 'Signup')
+    Signup = apps.lab.get_model("signups", "Signup")
     s = Signup.objects.last()
     s.created = timezone.now() - datetime.timedelta(hours=25)
     s.save()
 
-    if link := link_from_mail(address, 'Bevestiging'):
+    if link := link_from_mail(address, "Bevestiging"):
         page.goto(link)
-        expect(page.get_by_text('niet meer geldig')).to_be_visible()
+        expect(page.get_by_text("niet meer geldig")).to_be_visible()
     else:
-        pytest.fail('could not find validation link')
+        pytest.fail("could not find validation link")
 
     s = Signup.objects.last()
     assert s.email_verified is None
 
 
 def test_parent_login_expired(participant, apps, page, link_from_mail, login_as):
-    MailAuth = apps.lab.get_model('mailauth', 'MailAuth')
+    MailAuth = apps.lab.get_model("mailauth", "MailAuth")
 
-    page.goto(apps.parent.url + 'auth/')
+    page.goto(apps.parent.url + "auth/")
     page.fill('input[name="email"]', participant.email)
-    page.locator('button').get_by_text('Send').click()
+    page.locator("button").get_by_text("Send").click()
 
     mauth = MailAuth.objects.last()
     mauth.expiry = timezone.now() - datetime.timedelta(seconds=1)
     mauth.save()
 
-    link = link_from_mail(participant.email, 'Link')
+    link = link_from_mail(participant.email, "Link")
     page.goto(link)
-    expect(page.get_by_text('verlopen')).to_be_visible()
+    expect(page.get_by_text("verlopen")).to_be_visible()
 
-    expect(page.get_by_text('Appointments')).not_to_be_visible()
+    expect(page.get_by_text("Appointments")).not_to_be_visible()
 
 
 def test_parent_login_multiple_children(participant, apps, page, login_as):
@@ -256,6 +264,6 @@ def test_parent_login_multiple_children(participant, apps, page, login_as):
         email_subscription=True,
     )
     assert login_as(first.email) is True
-    page.locator('a').get_by_text(first.name).click()
-    page.wait_for_url('**/overview')
-    expect(page.locator('.alert-error')).to_have_count(0)
+    page.locator("a").get_by_text(first.name).click()
+    page.wait_for_url("**/overview")
+    expect(page.locator(".alert-error")).to_have_count(0)
